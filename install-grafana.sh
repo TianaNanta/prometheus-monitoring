4405debfc8fcc173f40e84d1744dd08cd32e4f71
TianaNanta <andrianjakananta@gmail.com>
2023-12-28 09:23:55 +0300

    Add musl package to Grafana installation

--- install-grafana.sh
+++ install-grafana.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y adduser libfontconfig1
+sudo apt-get install -y adduser libfontconfig1 musl
 wget https://dl.grafana.com/oss/release/grafana_10.2.3_amd64.deb
 sudo dpkg -i grafana_10.2.3_amd64.deb
 sudo systemctl daemon-reload
